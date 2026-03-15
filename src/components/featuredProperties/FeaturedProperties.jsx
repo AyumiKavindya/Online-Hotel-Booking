5caf152d54c152ff9401dc2bef309fd825d5894e
AyumiKavindya <ayumikavi@gmail.com>
2026-03-15 21:27:39 +0530

    Add featured properties

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -4,17 +4,16 @@ import HotelroomImg from "../../assets/hotelroom.jpg";
 const FeaturedProperties = () => {
   return (
     <div className="fp">
-
-    <div className="fpItem">
-        <img src={HotelroomImg} alt="" className="fpImg"/>
-        <span className="fpName">Aparthotel Stare Miasto</span>
-        <span className="fpCity">Madrid</span>
-        <span className="fpPrice">Starting from $120</span>
-        <div className="fpRating">
-            <button>8.9</button>
-            <span>Excellent</span>
+        <div className="fpItem">
+            <img src={HotelroomImg} alt="" className="fpImg"/>
+            <span className="fpName">Aparthotel Stare Miasto</span>
+            <span className="fpCity">Madrid</span>
+            <span className="fpPrice">Starting from $120</span>
+            <div className="fpRating">
+                <button>8.9</button>
+                <span>Excellent</span>
+            </div>
         </div>
-    </div>
 
     <div className="fpItem">
         <img src={HotelroomImg} alt="" className="fpImg"/>
